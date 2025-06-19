06271f555ef0b30e5c901406236888d60e2350fe
Matheus Oliveira <matheusoliveira353jr@gmail.com>
2025-06-18 22:06:23 -0300

    Update Apagar-led.ino

--- Jose de Assis/Apagar-led.ino
+++ Jose de Assis/Apagar-led.ino
@@ -13,3 +13,21 @@ void loop() {
   
 
 }
+
+---
+
+  /**
+  Pisca LED 
+  @author Matheus Pinheiro de Oliveira
+  @link https://github.com/matheuspoliveiraa
+*/
+
+void setup() {
+  pinMode (13, OUTPUT);
+  digitalWrite (13, 0);
+}
+
+void loop() {
+  
+
+}
