abaf8313b35844ce05e2d3f36097daee14e8a9c2
Matheus Oliveira <matheusoliveira353jr@gmail.com>
2025-06-18 22:10:01 -0300

    Update Piscar-led-sem-botão.ino

--- Jose de Assis/Piscar-led-sem-botão.ino
+++ Jose de Assis/Piscar-led-sem-botão.ino
@@ -4,13 +4,39 @@
   @link https://github.com/matheuspoliveiraa
 */
 
+//piscar apenas uma vez
+
 void setup() {
   pinMode (13, OUTPUT);
- 
+  digitalWrite (13, HIGH);
+  delay(1000); // 1000ms = 1 segundo
+  digitalWrite (13, LOW);
+  delay(1000);
 }
 
 void loop() {
-  digitalWrite (13, HIGH);
+  
 
 }
 
+---
+  
+  /**
+  Pisca LED 
+  @author Matheus Pinheiro de Oliveira
+  @link https://github.com/matheuspoliveiraa
+*/
+
+// piscar em looping
+  
+void setup() {
+  pinMode (13, OUTPUT);
+  digitalWrite (13, HIGH);
+}
+
+void loop() {
+
+  delay(1000); // 1000ms = 1 segundo
+  digitalWrite (13, LOW);
+  delay(1000);
+}
