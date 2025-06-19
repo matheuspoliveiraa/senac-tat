b40b8d7e338dcf897c99b9ed74f97f6b18bfe988
Matheus Oliveira <matheusoliveira353jr@gmail.com>
2025-06-18 22:00:15 -0300

    Update Piscar-led-sem-botão.ino

--- Jose de Assis/Piscar-led-sem-botão.ino
+++ Jose de Assis/Piscar-led-sem-botão.ino
@@ -1,1 +1,16 @@
+/**
+  Pisca LED 
+  @author Matheus Pinheiro de Oliveira
+  @link https://github.com/matheuspoliveiraa
+*/
+
+void setup() {
+  pinMode (13, OUTPUT);
+ 
+}
+
+void loop() {
+  digitalWrite (13, HIGH);
+
+}
 
