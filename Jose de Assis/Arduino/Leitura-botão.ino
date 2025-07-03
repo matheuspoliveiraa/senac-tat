4a1834e0d059424de1fe6a81e8877034c8c4c654
Matheus Oliveira <matheusoliveira353jr@gmail.com>
2025-07-03 20:05:11 -0300

    Create Leitura-botão.ino

---
+++ Jose de Assis/Arduino/Leitura-botão.ino
@@ -0,0 +1,18 @@
+/**
+  Pull-down (Leitura de um botão)
+  @author Matheus Pinheiro de Oliveira
+  @link https://www.tinkercad.com/things/cRlv9qXmwsu/editel?returnTo=%2Fdashboard
+
+*/
+
+void setup() {
+  Serial.begin(9600);
+  pinMode (2, INPUT); // configurando o pino 1 do Arduino como entrada
+
+
+}
+
+void loop() {
+  int botao = digitalRead(2); // Armazenar o valor do botão na variavel.
+  Serial.println(botao);
+}
